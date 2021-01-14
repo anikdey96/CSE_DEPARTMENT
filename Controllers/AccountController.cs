a991c32141e05e0924c1d3326195380bbff95698
Anik Dey Joy <anikdeyjoycse16@outlook.com>
2021-01-15 02:16:04 +0600

    14/01/21(Role-Selection)

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -184,8 +184,11 @@ namespace CSE_DEPARTMENT.Controllers
         {
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = model.Email, Email = model.Email };
+               
+               
+                var user = new ApplicationUser { UserName = model.Email, Email = model.Email};
                 var result = await UserManager.CreateAsync(user, model.Password);
+               
                 if (result.Succeeded)
                 {
                     await SignInManager.SignInAsync(user, isPersistent: false, rememberBrowser: false);
